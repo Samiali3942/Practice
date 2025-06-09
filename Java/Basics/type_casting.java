b8cff0e00abcb74e5ed9093cf8782cf1d400cc2d
unknown <samialimulla03@gmail.com>
2025-06-09 18:55:15 +0530

    Initial commit

---
+++ Java/Basics/type_casting.java
@@ -0,0 +1,13 @@
+public class type_casting {
+    public static void main(String[] args) {
+        
+        float a= 29.69f;
+        int b= (int) a;
+        System.out.println(b);
+    
+        char c='s';
+        int d=c;
+        System.out.println(d);
+    }
+        
+    }
