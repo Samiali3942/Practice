cb9c87c90f67034ca2680733d8ce5eb0bfe89dfa
unknown <samialimulla03@gmail.com>
2025-06-10 19:37:48 +0530

    Initial Commit

--- Java/Basics/practice.java
+++ Java/Basics/practice.java
@@ -3,7 +3,7 @@ public class practice {
     public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     char ch=sc.next().charAt(0);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+    if(ch=='a' || ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
         System.out.println("Vowel");
     }
     else{
