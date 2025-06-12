e5e3a742fca2d3be8c4d14a829303a0c1bd9cbf9
unknown <samialimulla03@gmail.com>
2025-06-13 00:55:35 +0530

    While loop

---
+++ Java/Basics/loops3.java
@@ -0,0 +1,21 @@
+// while loop
+// print the square of the first 5 numbers
+
+class loops3
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int x=1;
+		int count=1;
+		while(count<=5){
+		   
+		   int num = x*x;
+		   System.out.print(num + " "); 
+		   x++;
+		   count++;
+		}
+
+	}
+}
+// 1 4 9 16 25 
