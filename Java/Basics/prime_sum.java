e4d9e5f600c541ace764a76860c735c2c583b891
unknown <samialimulla03@gmail.com>
2025-06-18 21:49:25 +0530

    Sum of N prime numbers

--- Java/Basics/prime_sum.java
+++ Java/Basics/prime_sum.java
@@ -1,25 +1,25 @@
 
 
 public class prime_sum {
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
+    public static boolean isPrime(int n) { //2
+        if (n <= 1) { //2<=1
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <= Math.sqrt(n); i++) { //2<=sqrt(2)
+            if (n % i == 0) { //2%2==0
                 return false;
             }
-        }
-        return true;
+        } //2<=2
+        return true; //true
     }
     public static void main(String[] args) {
-        int n = 10;
-        int sum = 0;
-        for (int i = 2; i <= n; i++) {
-            if (isPrime(i)) {
-                sum += i;
+        int n = 10; //10
+        int sum = 0; //0
+        for (int i = 2; i <= n; i++) { //2<=10
+            if (isPrime(i)) { //isPrime(2)
+                sum += i; //sum=0+2=2
             }
-        }
-        System.out.println(sum);
+        } //2<=10
+        System.out.println(sum); //2
     }
 }
